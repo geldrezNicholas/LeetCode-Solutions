3d50dba990fb3ffc970e8efd94b932ce564e919f
Nicholas <geldrez.nicholas@gmail.com>
2026-01-11 18:30:57 -0500

    Time: 0 ms (100%), Space: 20.7 MB (7.88%), Difficulty: Easy

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        L, R = 0, len(nums)-1
+
+        if target < nums[0] or target > nums[R]:
+            return -1
+        
+        while L <= R:
+            M = (L+R)//2
+
+            if nums[M] < target:
+                L = M+1
+            elif nums[M] > target:
+                R = M-1
+            else: 
+                return M
+        return -1
