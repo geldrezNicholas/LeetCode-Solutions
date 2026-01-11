87feb99329a8d0979418a4c5ed56f5b56872a716
Nicholas <geldrez.nicholas@gmail.com>
2026-01-11 07:00:52 -0500

    Time: 3 ms (17.02%), Space: 19.5 MB (5.31%), Difficulty: Easy

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        c = 0
+        while c < len(students):
+            if sandwiches[0] == students[0]:
+                sandwiches.pop(0) 
+                students.pop(0)
+                c = 0
+            else:
+                students.append(students.pop(0))
+                c+=1
+        return len(students)
