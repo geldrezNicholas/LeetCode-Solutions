d56d3e84dd213bcb66ece642816701ac326cad9a
Nicholas <geldrez.nicholas@gmail.com>
2026-06-11 15:29:32 -0400

    Time: 52 ms (67.94%), Space: 22.5 MB (85.6%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,16 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        pos = 0
-        dict = {}
-        curr = head
-        while curr != None:
-            if curr not in dict:
-                dict[curr] = pos
-            elif curr in dict:
-                if pos > dict[curr]:
-                    return True
-            pos+=1
-            curr = curr.next
+        fast=slow=head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
         return False
         
